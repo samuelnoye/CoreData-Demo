9d204cd29eee3b49e00867670c47ec1272ebb6bd
Samuel Noye <samuel.noye@amalitech.org>
2022-03-31 16:23:13 +0000

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-Demo
+//
+//  Created by Samuel Noye on 31/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
